48afbccf928aff15021face93c3bf773d58ac9f1
Arnaud <56339879+ArnaudGuibert@users.noreply.github.com>
2020-10-26 17:38:29 +0100

    Update log.py

--- Serveur/log.py
+++ Serveur/log.py
@@ -3,13 +3,13 @@ import os, datetime
 
 def create_log_txt(liste, result):
     # Entrees :
-    # les notes en format "1/3/5/7/9"
-    # la sortie process_marks_raw() du calcul des notes
+    # liste = les notes en format "1/3/5/7/9"
+    # result = la sortie raw_result de process_marks()
     now = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-
+    liste = liste.replace("/", " ")
+    
     fichier_log = open("fichier_log.txt", "w")
     fichier_log.write(now + "\n")
-    liste = liste.replace("/", " ")
     fichier_log.write(liste + "\n")
     fichier_log.write(result + "\n")
     fichier_log.close()
