a6c53d76243030d1cdf7ea45f15545500eab94b8
Reimu <remihuguenot89@gmail.com>
2020-10-26 18:20:51 +0100

    ajout de logs dans serv.py

--- Serveur/serv.py
+++ Serveur/serv.py
@@ -1,7 +1,27 @@
 import boto3
 import sys
+import os, datetime
 
 
+""" Create log file for AWS bucket """
+
+
+def create_log_txt(liste, result):
+    # Entrees :
+    # liste = les notes en format "1/3/5/7/9"
+    # result = la sortie raw_result de process_marks()
+    now = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    liste = liste.replace("/", " ")
+    
+    fichier_log = open("fichier_log.txt", "w")
+    fichier_log.write(now + "\n")
+    fichier_log.write(liste + "\n")
+    fichier_log.write(result + "\n")
+    fichier_log.close()
+
+
+def delete_log_txt():
+    os.remove("fichier_log.txt")
 
 def parse(input):
     # input is a string
@@ -88,7 +108,9 @@ while 1 :
 
         # affichage du resultat
         print('MESSAGE , {0}{1}'.format(message.body, result[0]))
-            
+           
+        #Creation des logs
+        create_log_txt(message.body, result[1])
            
         #message has been processed it can be  remove to process others messages
         message.delete()
