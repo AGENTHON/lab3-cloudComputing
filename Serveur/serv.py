fd90495601833e9168835a7e8564ae942bf53eed
Reimu <remihuguenot89@gmail.com>
2020-10-26 17:58:07 +0100

    new ver serv.py

--- Serveur/serv.py
+++ Serveur/serv.py
@@ -1,7 +1,27 @@
 import boto3
+import sys
+
+
+
+def parse(input):
+    # input is a string
+    # output is a list of floats
+
+    liste = input.split("/")
+    list_marks = []
+    for mark in liste:
+        try:
+            float_mark = float(mark)
+            list_marks.append(float_mark)
+        except ValueError:
+            print("FunctionError: Non-Float Argument")
+            sys.exit(1)
+    
+    return list_marks
 
 
 """ First part with EC2 and a Python program """
+
 def process_marks(marks_list):
     # input : marks_list = list of marks (as float) of students
     # output : mean, median, max, min of given marks
@@ -27,9 +47,12 @@ def process_marks(marks_list):
     resultat += "Min: " + str(min_mark) + "\n"
     resultat += "Mean: " + str(mean) + "\n"
     resultat += "Median: " + str(median)
-    return resultat
-
-
+    
+    # output raw_result
+    raw_result = "Max: " + str(max_mark) + " Min: " + str(min_mark) + " Mean: " + str(mean) + " Median: " + str(median)
+    
+    # return strings
+    return [resultat, raw_result]
 
 
 
@@ -41,38 +64,42 @@ def process_marks(marks_list):
 sqs = boto3.resource('sqs')
 
 try:
-        queue = sqs.get_queue_by_name(QueueName= "requestQueue.fifo")
-        print("Queue exist yeah")
-        
-        #Récupération et traitement des info en continu
-        while 1 :
-            # Process messages by printing out body and optional author name
-            for message in queue.receive_messages(MessageAttributeNames=['Author']):
-                # Get the custom author message attribute if it was set
-                author_text = ''
-                if message.message_attributes is not None:
-                    author_name = message.message_attributes.get('Author').get('StringValue')
-                    if author_name:
-                        author_text = ' ({0})'.format(author_name)
-
-                # Print out the body and author (if set)
-                print('MESSAGE , {0}{1}'.format(message.body, author_text))
-                
-                #calcul du resultat
-                result = process_marks(message.body)
-                #message has been processed it can be  remove to process others messages
-                message.delete()
-                
-                #renvoyer  le resultat :
-                
-                #création d'une nouvelle queue :
-                Rqueue = sqs.create_queue(QueueName="responseQueue.fifo", Attributes={'FifoQueue': 'true', 'DelaySeconds': '5', 'ContentBasedDeduplication': 'true'})
-                
-                #envoi du message
-                response = Rqueue.send_message(MessageBody=result, MessageGroupId='messages')
-                
+    queue = sqs.get_queue_by_name(QueueName= "requestQueue.fifo")
+    print("Queue exist yeah")
 
 except:
-        print("queue not found ahhhh")
+    print("queue not found ahhhh")
+    
+    
+    
+#Récupération et traitement des info en continu
+while 1 :
+# Process messages by printing out body and optional author name
+    for message in queue.receive_messages(MessageAttributeNames=['Author']):
+    # Get the custom author message attribute if it was set
+        author_text = ''
+        if message.message_attributes is not None:
+            author_name = message.message_attributes.get('Author').get('StringValue')
+            if author_name:
+                author_text = ' ({0})'.format(author_name)
+
+        #calcul du resultat
+        result = process_marks(parse(message.body))
+
+        # affichage du resultat
+        print('MESSAGE , {0}{1}'.format(message.body, result[0]))
+            
+           
+        #message has been processed it can be  remove to process others messages
+        message.delete()
+            
+        #renvoyer  le resultat :
+            
+        #création d'une nouvelle queue :
+        Rqueue = sqs.create_queue(QueueName="responseQueue.fifo", Attributes={'FifoQueue': 'true', 'DelaySeconds': '5', 'ContentBasedDeduplication': 'true'})
+            
+        #envoi du message
+        response = Rqueue.send_message(MessageBody=result[0], MessageGroupId='messages')
+            
 
 
